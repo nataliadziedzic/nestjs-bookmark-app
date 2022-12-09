192acf478eb7afd1b5ed85fca71ef5b566263078
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2022-12-09 09:54:21 +0100

    feat: add docker scripts for restarting the containers and deploying prisma migrations, also add nest config module for env variables

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    console.log('config', config.get('DATABASE_URL'));
   }
 }
