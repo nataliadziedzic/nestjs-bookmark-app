b81eb0b3c3b3a62d5a0d3db0905c5f5dfde55335
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2022-12-10 14:32:28 +0100

    feat: add custom auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,19 +2,20 @@ import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { ParseIntPipe } from '@nestjs/common/pipes';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('myAccount')
   getLoggedInUser(@Req() req: Request) {
     return req.user;
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get(':id')
   getUserById(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findUser(id);
