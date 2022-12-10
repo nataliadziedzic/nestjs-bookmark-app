280d4ab7d6219d5fedb8c4113e2ee15f6cc8dd4e
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2022-12-10 14:43:51 +0100

    feat: update user decorator to return either entire user or 1 field from user object

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ParseIntPipe } from '@nestjs/common/pipes';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
