d90a9b3688dfd806efc67efebc3c5966282d70ff
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2022-12-18 15:57:57 +0100

    feat: add jwt validate function payload type

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: { sub: number; email: string }) {
     const user = await this.prisma.user.findUnique({
       where: {
         id: payload.sub,
